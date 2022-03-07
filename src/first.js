cb98c95534f4003d7d8809190783bb92256889a6
Ben Grynhaus <me@bengr.dev>
2022-03-07 14:48:17 +0200

    initial commit - issue reproduction

---
+++ src/first.js
@@ -0,0 +1,4 @@
+import { second1 } from "./second";
+
+export const first1 = "first" + second1;
+export const first2 = "first2";
---
+++ src/second.js
@@ -0,0 +1,4 @@
+import { first1 } from "./first";
+
+export const second1 = "second1";
+export const second2 = "second2" + first1;
