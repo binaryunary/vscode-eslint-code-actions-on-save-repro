b68eccfc311a14287603cbfbc227b3ffee861f33
Erik Mägi <magi@axinom.com>
2022-06-21 15:25:38 +0300

    initial setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,13 @@
 module.exports = {
   root: true,
+  env: {
+    node: true
+  },
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint", "import"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   rules: {
+    "import/no-unresolved": "error",
     "import/no-cycle": "error",
   },
 };
--- src/first.js
+++ src/first.js
@@ -1,4 +1,5 @@
 import { second1 } from "./second";
+import { bla } from './bla'
 
 export const first1 = "first" + second1;
-export const first2 = "first2";
+export const first2 = { first2: "first2" };
